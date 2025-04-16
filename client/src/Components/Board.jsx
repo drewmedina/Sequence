f5045cfdfd58256eda3db611a59a04cd5946b21c
drewmedina <ofc10drew@gmail.com>
2025-04-16 15:52:38 -0500

    Increase board size, work on turn logic

--- client/src/Components/Board.jsx
+++ client/src/Components/Board.jsx
@@ -1,72 +1,86 @@
-import React from 'react';
-import Card from './GamePieces/Card';
+import React from "react";
+import Card from "./GamePieces/Card";
+import styled from "styled-components";
+import { useAuth } from "../Auth/AuthContext";
 
-function DefaultGameBoard (){
-   const boardData = [
-    ["FX", "A♦", "K♦", "Q♦", "10♦", "9♦", "8♦", "7♦", "6♦", "FX"],
-    ["A♣", "7♠", "6♠", "5♠", "4♠", "3♠", "2♠", "2♥", "3♥", "5♦"],
-    ["K♣", "8♠", "10♣", "Q♣", "K♣", "A♣", "A♦", "K♦", "4♥", "4♦"],
-    ["Q♣", "9♠", "9♣", "8♥", "9♥", "10♥", "Q♥", "Q♦", "5♥", "3♦"],
-    ["10♣", "10♠", "8♣", "7♥", "2♥", "3♥", "K♥", "10♦", "6♥", "2♦"],
-    ["9♣", "Q♠", "7♣", "6♥", "5♥", "4♥", "A♥", "9♦", "7♥", "A♠"],
-    ["8♣", "K♠", "6♣", "5♣", "4♣", "3♣", "2♣", "8♦", "8♥", "K♠"],
-    ["7♣", "A♠", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "9♥", "Q♠"],
-    ["6♣", "5♣", "4♣", "3♣", "2♣", "A♥", "K♥", "Q♥", "10♥", "10♠"],
-    ["FX", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "FX"],
-  ]
-  
-  return(
-    
-     <div style={{
-      display: "grid", 
-      gridTemplateColumns: "repeat(10, 1fr)", 
-      columnGap: "3px", 
-      rowGap: "14px",
-      width: "610px",
-      height: "750px",
-      transform: "scale(0.35) rotate(90deg)", 
-      backgroundRepeat: "no-repeat",
-      transformOrigin: "center",
-    }}>
-        {
-    boardData.map((row) =>
-    row.map((value) => {
-      if (value === "FX") {
-        <div></div>; // Keep "FREE" as text
-      }
-  
-      const rank = value.slice(0, -1); // Extract number/face (e.g., "10", "K")
-      const suitSymbol = value.slice(-1); // Extract suit symbol (♥, ♦, ♠, ♣)
-  
-      // Convert suit symbols to full suit names & folder structure
-      const suitMap = {
-        "♥": "hearts",
-        "♦": "diamonds",
-        "♠": "spades",
-        "♣": "clubs",
-        "X": "frees"
-      };
-      const suitFolder = suitMap[suitSymbol]; // Folder name (e.g., "Hearts")
-  
-      // Convert face card abbreviations to full names
-      
-      const rankMap = { 2: "Two", 3: "Three" ,4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten", "A": "Ace", "K": "King", "Q": "Queen", "F": "Free"};
-      const formattedRank = rankMap[rank] || rank; // Use full name for face cards, else keep number
-  
-      // return {
-      //   value,
-      //   type: ["♠", "♣"].includes(suitSymbol) ? "black" : "red",
-      //   image: `./images/Cards/${suitFolder}/${formattedRank}_of_${suitFolder.toLowerCase()}.png`, // Correct path
-      //   token: null // Track player token (null = empty)
-      // };
-     return <Card rank={formattedRank} suit={suitFolder}></Card>
-    })
-)}
-</div>
+const HoverCard = styled.div`
+  transition: transform 0.3s;
+  &:hover {
+    transform: translateY(-10px); /* Makes the card lift slightly on hover */
+  }
+`;
 
+function DefaultGameBoard({ boardData, setSelectedCard }) {
+  const { currentUser } = useAuth();
 
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(10, 1fr)",
+        columnGap: "3px",
+        rowGap: "14px",
+        transform: "scale(.6) rotate(90deg)",
+        backgroundRepeat: "no-repeat",
+        transformOrigin: "center",
+      }}
+    >
+      {boardData.map((row, rowIndex) =>
+        row.map((value, colIndex) => {
+          const [cardValue, token] = value.split("#");
+          const rank = cardValue.slice(0, -1); // Extract number/face (e.g., "10", "K")
+          const suitSymbol = cardValue.slice(-1); // Extract suit symbol (♥, ♦, ♠, ♣)
+          console.log(token);
+          // Convert suit symbols to full suit names & folder structure
+          const suitMap = {
+            "♥": "hearts",
+            "♦": "diamonds",
+            "♠": "spades",
+            "♣": "clubs",
+            X: "frees",
+          };
+          const suitFolder = suitMap[suitSymbol]; // Folder name (e.g., "Hearts")
+
+          // Convert face card abbreviations to full names
+
+          const rankMap = {
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+            10: "Ten",
+            A: "Ace",
+            K: "King",
+            Q: "Queen",
+            F: "Free",
+          };
+          const formattedRank = rankMap[rank] || rank;
+          return (
+            <HoverCard
+              key={rowIndex - colIndex}
+              onClick={() => {
+                setSelectedCard({
+                  row: rowIndex,
+                  col: colIndex,
+                  user: currentUser.username,
+                });
+              }}
+            >
+              <Card
+                rank={formattedRank}
+                suit={suitFolder}
+                token={`#${token}` || null}
+              ></Card>
+            </HoverCard>
+          );
+        })
+      )}
+    </div>
   );
-  
 }
 
 export default DefaultGameBoard;
--- client/src/Pages/Play.jsx
+++ client/src/Pages/Play.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { getCard, removeCardFromDeck, drawCard } from "../Components/GamePieces/Deck";
-import Card from "../Components/GamePieces/Card"
-import Hand from "../Components/GamePieces/Hand"; 
-import BoardImage from "../Components/BoardImage"; 
+import {
+  getCard,
+  removeCardFromDeck,
+  drawCard,
+} from "../Components/GamePieces/Deck";
+import Card from "../Components/GamePieces/Card";
+import Hand from "../Components/GamePieces/Hand";
+import BoardImage from "../Components/BoardImage";
 import DefaultGameBoard from "../Components/Board";
-
+import Avatar from "antd/es/avatar/Avatar";
+import { UserOutlined } from "@ant-design/icons";
 
 //a div styled as pink and also centers everything in it when used
 const AppContainer = styled.div`
@@ -20,14 +25,11 @@ const AppContainer = styled.div`
   align-items: center;
 `;
 
-
-
 const CardContainer = styled.div`
   display: flex;
   justify-content: center;
   gap: 0px;
   margin-bottom: 50px;
-
 `;
 
 const BoardContainer = styled.div`
@@ -35,29 +37,29 @@ const BoardContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 980px;
-  height: 750px; 
-  margin-top: -100px;
-  margin-left: 380px;
+  width: 100%;
+  height: 100%;
+  min-height: 600px;
 `;
 
 const StyledBoardImage = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  position: relative;
+  /* top: 0; */
+  /* left: 0; */
+  width: 80%;
+  height: 85%;
   z-index: 1;
   pointer-events: none;
 `;
 
 const StyledGameBoard = styled.div`
   position: absolute;
-  width: 100%;
-  height: 100%;
+  width: 80%;
+  height: 85%;
   z-index: 2;
-  margin-bottom: 110px;
-  margin-left: 202px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const HoverCard = styled.div`
@@ -71,25 +73,36 @@ const ButtonContainer = styled.div`
   height: 40px;
   margin-top: 10px;
   margin-bottom: -80px;
-  z-index: 3;  /* z index is 3 because otherwise the buttons can't be clicked, the board covers it.*/
-
+  z-index: 3; /* z index is 3 because otherwise the buttons can't be clicked, the board covers it.*/
 `;
-
-
-
-
-
+const startingBoard = [
+  ["FX", "A♦", "K♦", "Q♦", "10♦", "9♦", "8♦", "7♦", "6♦", "FX"],
+  ["A♣", "7♠", "6♠", "5♠", "4♠", "3♠", "2♠", "2♥", "3♥", "5♦"],
+  ["K♣", "8♠", "10♣", "Q♣", "K♣", "A♣", "A♦", "K♦", "4♥", "4♦"],
+  ["Q♣", "9♠", "9♣", "8♥", "9♥", "10♥", "Q♥", "Q♦", "5♥", "3♦"],
+  ["10♣", "10♠", "8♣", "7♥", "2♥", "3♥", "K♥", "10♦", "6♥", "2♦"],
+  ["9♣", "Q♠", "7♣", "6♥", "5♥", "4♥", "A♥", "9♦", "7♥", "A♠"],
+  ["8♣", "K♠", "6♣", "5♣", "4♣", "3♣", "2♣", "8♦", "8♥", "K♠"],
+  ["7♣", "A♠", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "9♥", "Q♠"],
+  ["6♣", "5♣", "4♣", "3♣", "2♣", "A♥", "K♥", "Q♥", "10♥", "10♠"],
+  ["FX", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "FX"],
+];
 //gap: 1rem; /* Adjust space between cards */
 function Play() {
-  
   const hand = Hand();
   const [cards, setCards] = useState(hand.getCards());
-
-  //gets a new random card
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [board, setBoard] = useState(startingBoard);
   const setRandomCard = () => {
-      setCards([
-        getCard(), getCard(), getCard(), getCard(), getCard(), getCard(), getCard()
-      ]);
+    setCards([
+      getCard(),
+      getCard(),
+      getCard(),
+      getCard(),
+      getCard(),
+      getCard(),
+      getCard(),
+    ]);
   };
 
   const [discardPile, setDiscardPile] = useState([]);
@@ -104,43 +117,75 @@ function Play() {
     const newCard = drawCard(); // Draw a card from the deck
     setCards((prevCards) => [...prevCards, newCard]); // Add the new card to the hand
   };
-
-return (
-  
-  <AppContainer>
-    <ButtonContainer>
-          <button type="button" onClick={setRandomCard}> New Cards </button>
-          <button type="button" onClick={drawCardFromDeck}> Draw Card </button>
-
+  useEffect(() => {
+    if (selectedCard) {
+      console.log("hello", selectedCard);
+      var new_board = board;
+      new_board[selectedCard.row][selectedCard.col] =
+        board[selectedCard.row][selectedCard.col] + "#000000";
+      setBoard(new_board);
+    }
+  }, [selectedCard]);
+  return (
+    <AppContainer>
+      <ButtonContainer>
+        <button type="button" onClick={setRandomCard}>
+          {" "}
+          New Cards{" "}
+        </button>
+        <button type="button" onClick={drawCardFromDeck}>
+          {" "}
+          Draw Card{" "}
+        </button>
       </ButtonContainer>
-    <BoardContainer>
-      <StyledBoardImage>
-        <BoardImage />
-      </StyledBoardImage>
-    
-      <StyledGameBoard>
-          <DefaultGameBoard />
-      </StyledGameBoard>
-    </BoardContainer>
+      <div
+        style={{
+          height: "100%",
+          width: "90%",
+          display: "flex",
+          direction: "row",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          size="large"
+          icon={<UserOutlined />}
+          style={{ backgroundColor: "#87d068", cursor: "pointer" }}
+        />
+        <BoardContainer>
+          <StyledBoardImage>
+            <BoardImage />
+          </StyledBoardImage>
+          <StyledGameBoard>
+            <DefaultGameBoard
+              boardData={board}
+              setSelectedCard={setSelectedCard}
+            />
+          </StyledGameBoard>
+        </BoardContainer>
+        <Avatar
+          size="large"
+          icon={<UserOutlined />}
+          style={{ backgroundColor: "#87d068", cursor: "pointer" }}
+        />
+      </div>
 
-    <div>
-         
-       
-       {/* <Card rank={card.rank} suit={card.suit} /> */}
-       
-
-       <CardContainer>
+      <div>
+        <CardContainer>
           {cards.map((card, index) => (
-            <HoverCard key = {index}>
-            <div onClick={() => discardCard(index)} style={{ cursor: "pointer" }}>
-            <Card key = {index} rank={card.rank} suit={card.suit} />
-            </div>
+            <HoverCard key={index}>
+              <div
+                onClick={() => discardCard(index)}
+                style={{ cursor: "pointer" }}
+              >
+                <Card key={index} rank={card.rank} suit={card.suit} />
+              </div>
             </HoverCard>
           ))}
         </CardContainer>
-     </div>
-  </AppContainer>
-);
- }
+      </div>
+    </AppContainer>
+  );
+}
 
-export default Play
+export default Play;
