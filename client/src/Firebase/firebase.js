e861910e0b60ec03c2be8ebbf620248beafb1031
maitreyipurandare <143023357+maitreyipurandare@users.noreply.github.com>
2025-05-04 18:04:39 -0500

    env stuff

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtMP66J_vu0W1snbhRnh3rVIY8pceIPew",
-  authDomain: "sequence-3bc30.firebaseapp.com",
-  projectId: "sequence-3bc30",
-  storageBucket: "sequence-3bc30.firebasestorage.app",
-  messagingSenderId: "580469336528",
-  appId: "1:580469336528:web:a9eb49d5bce5b5de3771d5",
-  measurementId: "G-1HXER070H6",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }; 
 
 //Change the above to your firebase config
