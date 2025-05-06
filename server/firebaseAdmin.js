2e3d11060d7c81994d8a6cd9da10a94e46abb376
drewmedina <ofc10drew@gmail.com>
2025-05-06 15:08:19 -0500

    .env error

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,11 +1,18 @@
 // server/firebaseAdmin.js
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+import dotenv from "dotenv";
+
+dotenv.config();
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 export const db = admin.firestore();
